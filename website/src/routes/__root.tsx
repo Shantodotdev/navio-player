dce7b5d284210abb21ded80de298478bbd992457
KR Shanto <krshanto2005@gmail.com>
2026-07-16 17:26:48 +0600

    feat(website): remove old favicon and logos, update icon reference in __root.tsx

--- website/src/routes/__root.tsx
+++ website/src/routes/__root.tsx
@@ -30,6 +30,11 @@ export const Route = createRootRoute({
         rel: "stylesheet",
         href: appCss,
       },
+      {
+        rel: "icon",
+        type: "image/png",
+        href: "/navio-logo.png",
+      },
     ],
   }),
   shellComponent: RootDocument,
